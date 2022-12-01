635566b2b50cc557fdff8bf01b14739cf0b967ea
Mario Ravalli <mario.ravalli@bakeca.it>
2022-12-01 21:51:25 +0100

    first day, second iteration

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -6,19 +6,37 @@ pub fn part_one(input: &str) -> Option<u32> {
             if this_elv > strong_elv {
                 strong_elv = this_elv;
             }
-
             this_elv = 0;
-            continue
         } else {
             this_elv += line.parse::<i32>().unwrap();
         }
-
     }
     Some(strong_elv as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut first_three = [0, 0, 0];
+    let mut this_elv = 0;
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            first_three.sort();
+            if this_elv > first_three[0] {
+                first_three[0] = this_elv;
+            } else if this_elv > first_three[1] {
+                first_three[0] = first_three[1];
+                first_three[1] = this_elv;
+            } else if this_elv > first_three[2] {
+                first_three[0] = first_three[1];
+                first_three[1] = first_three[2];
+                first_three[2] = this_elv;
+            }
+            this_elv = 0;
+        } else {
+            this_elv += line.parse::<i32>().unwrap();
+        }
+    }
+
+    Some(first_three.iter().sum::<i32>() as u32)
 }
 
 fn main() {
@@ -40,6 +58,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }
