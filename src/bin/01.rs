336dc23b7e2e13f0b1d9791f130faba56d8978bd
Mario Ravalli <mario.ravalli@bakeca.it>
2022-12-01 10:09:37 +0100

    fixed return for test

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -34,7 +34,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
