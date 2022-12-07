58b815d099503209aedff58a230679eb0edf908f
Mario Ravalli <mario.ravalli@bakeca.it>
2022-12-07 02:47:44 +0100

    day 5 and 6 and removed clippy

---
+++ src/bin/05.rs
@@ -0,0 +1,100 @@
+extern crate core;
+
+use std::collections::VecDeque;
+
+pub fn part_one(input: &str) -> Option<String> {
+    let puzzle = input.split_terminator("\n\n").collect::<Vec<&str>>();
+    let mut stack = get_stack(puzzle[0]);
+
+    puzzle[1].lines().for_each(|line|{
+        let param:Vec<&str> = line.split_whitespace().collect();
+        rearrange9000(&mut stack, param[1], param[3], param[5]);
+    });
+
+    let message = get_message(&mut stack);
+
+    Some(message)
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    let puzzle = input.split_terminator("\n\n").collect::<Vec<&str>>();
+    let mut stack = get_stack(puzzle[0]);
+
+    puzzle[1].lines().for_each(|line|{
+        let param:Vec<&str> = line.split_whitespace().collect();
+        rearrange9001(&mut stack, param[1], param[3], param[5]);
+    });
+
+    let message = get_message(&mut stack);
+
+    Some(message)
+}
+
+pub fn rearrange9000(stack: &mut Vec<VecDeque<&str>>, qty: &str, from: &str, to: &str) {
+    for _i in 0.. qty.parse::<u8>().unwrap() {
+        let rack = stack[from.parse::<usize>().unwrap() -1].pop_back();
+        stack[to.parse::<usize>().unwrap() -1].push_back(rack.unwrap());
+    }
+}
+pub fn rearrange9001(stack: &mut Vec<VecDeque<&str>>, qty: &str, from: &str, to: &str) {
+    let mut temp: VecDeque<&str> = VecDeque::new();
+    for _i in 0.. qty.parse::<u8>().unwrap() {
+        let rack = stack[from.parse::<usize>().unwrap() -1].pop_back();
+        temp.push_front(rack.unwrap());
+    }
+    for _i in 0.. qty.parse::<u8>().unwrap() {
+        let rack = temp.pop_front();
+        stack[to.parse::<usize>().unwrap() -1].push_back(rack.unwrap());
+    }
+}
+
+pub fn get_message<'a>(stack: &'a mut Vec<VecDeque<&'a str>>) -> String {
+    let mut message = String::new();
+    for item in stack {
+        let aa = item.pop_back();
+        if !aa.is_none() {
+            message.push_str(aa.unwrap());
+        }
+    }
+    message
+}
+
+pub fn get_stack(puzzle: &str) -> Vec<VecDeque<&str>> {
+    puzzle.lines().fold(
+        vec![VecDeque::new(); 9],
+        |mut rack, line| {
+            for i in 0..9 {
+                let crane = line.get((i*4+1)..(i*4+2));
+                if let Some(crane) = crane {
+                    if !crane.trim().is_empty() && crane.trim().parse::<u8>().is_err() {
+                        rack[i].push_front(crane);
+                    }
+                }
+            }
+            rack
+        }
+    )
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 5);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 5);
+        assert_eq!(part_one(&input), Some("CMZ".to_string()));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 5);
+        assert_eq!(part_two(&input), Some("MCD".to_string()));
+    }
+}
---
+++ src/bin/06.rs
@@ -0,0 +1,58 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    for i in 0..input.len() -2 {
+        let crane = input.get(i..(i+4)).unwrap().to_string();
+        if !&crane[0..3].contains(&crane[3..4]) &&
+            !&crane[0..2].contains(&crane[2..3]) &&
+            &crane[0..1] != &crane[1..2] {
+            println!("{:?}", crane);
+            return Some((i + 4) as u32);
+        }
+    }
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    for i in 0..input.len() -12 {
+        let crane = input.get(i..(i+14)).unwrap().to_string();
+        if !&crane[0..13].contains(&crane[13..14]) &&
+            !&crane[0..12].contains(&crane[12..13]) &&
+            !&crane[0..11].contains(&crane[11..12]) &&
+            !&crane[0..10].contains(&crane[10..11]) &&
+            !&crane[0..9].contains(&crane[9..10]) &&
+            !&crane[0..8].contains(&crane[8..9]) &&
+            !&crane[0..7].contains(&crane[7..8]) &&
+            !&crane[0..6].contains(&crane[6..7]) &&
+            !&crane[0..5].contains(&crane[5..6]) &&
+            !&crane[0..4].contains(&crane[4..5]) &&
+            !&crane[0..3].contains(&crane[3..4]) &&
+            !&crane[0..2].contains(&crane[2..3]) &&
+            &crane[0..1] != &crane[1..2] {
+            println!("{:?}", crane);
+            return Some((i + 14) as u32);
+        }
+    }
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_one(&input), Some(10));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), Some(29));
+    }
+}
